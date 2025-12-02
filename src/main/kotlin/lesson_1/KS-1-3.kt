eab4f970db166f180f3c142ab440767a878119d4
NukeKB <alexey-seo@clientprosto.ru>
2025-12-02 12:18:25 +0300

    KS-1-4 еределал на строковый тип данных, хотя попробовал ради интереса подставить целые числа, форматирование прошло нормально https://skrinshoter.ru/sYsz11LmNkz?a

--- src/main/kotlin/lesson_1/KS-1-3.kt
+++ src/main/kotlin/lesson_1/KS-1-3.kt
@@ -2,12 +2,15 @@ package org.example.lesson_1
 
 fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "9"
+    var minute: String = "7"
+
     println(year)
-    println("%02d".format(hour))
-    println("%02d".format(minute))
-    hour = 10
-    minute = 55
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
     print("$hour:$minute")
 }
