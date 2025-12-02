a9a656e7ca1f42e5d287bba736c4bf4bccd655f7
NukeKB <alexey-seo@clientprosto.ru>
2025-12-02 09:16:08 +0300

    KS-1-1 Поправил ошибки после замечаний

--- src/main/kotlin/KotLearn_sprint_1/Lesson_1_peremennye.kt
+++ src/main/kotlin/KotLearn_sprint_1/Lesson_1_peremennye.kt
@@ -2,5 +2,5 @@ package org.example.KotLearn_sprint_1
 
 fun main() {
     val counterOfOrders: Int = 75
-    val succesMessage: String = "Мы уже собираем ваш заказ, спасибо за покупку!"
+    val succesMessage: String = "Мы уже собираем заказ, спасибо вам за покупку!"
 }
