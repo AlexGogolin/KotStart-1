267da70bb88f249ea8cdbea5da4607ad1868d74b
NukeKB <alexey-seo@clientprosto.ru>
2025-12-03 11:22:46 +0300

    KS-1-5 поправил константы на UPPER_SNAKE_CASE

--- src/main/kotlin/KotLearn_sprint_1/KS-1-5.kt
+++ src/main/kotlin/KotLearn_sprint_1/KS-1-5.kt
@@ -1,14 +1,14 @@
 package org.example.KotLearn_sprint_1
 
-const val secondsInHour: Short = 3600
-const val secondsInMinutes: Byte = 60
+const val SECONDS_IN_HOUR: Short = 3600
+const val SECONDS_IN_MINUTES: Byte = 60
 
 fun main() {
     val totalSeconds: Short = 6480
-    val hours: Int = totalSeconds / secondsInHour
-    val remaingSeconds: Int = totalSeconds % secondsInHour
-    val minutes: Int = remaingSeconds / secondsInMinutes
-    val seconds: Int = remaingSeconds % secondsInMinutes
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val remaingSeconds: Int = totalSeconds % SECONDS_IN_HOUR
+    val minutes: Int = remaingSeconds / SECONDS_IN_MINUTES
+    val seconds: Int = remaingSeconds % SECONDS_IN_MINUTES
 
     print("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
