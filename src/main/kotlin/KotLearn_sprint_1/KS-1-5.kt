7206abcd5255adc8dd431f95948bb6419f79e7ae
NukeKB <alexey-seo@clientprosto.ru>
2025-12-03 09:41:28 +0300

    KS-1-5 Спасибо что сказал изменить на const val , благодаря этому я понял как их правильно использовать:)

--- src/main/kotlin/KotLearn_sprint_1/KS-1-5.kt
+++ src/main/kotlin/KotLearn_sprint_1/KS-1-5.kt
@@ -1,14 +1,14 @@
 package org.example.KotLearn_sprint_1
 
 const val secondsInHour: Short = 3600
-const val secondsInMinute: Byte = 60
+const val secondsInMinutes: Byte = 60
 
 fun main() {
     val totalSeconds: Short = 6480
     val hours: Int = totalSeconds / secondsInHour
     val remaingSeconds: Int = totalSeconds % secondsInHour
-    val minutes: Int = remaingSeconds / secondsInMinute
-    val seconds: Int = remaingSeconds % secondsInMinute
+    val minutes: Int = remaingSeconds / secondsInMinutes
+    val seconds: Int = remaingSeconds % secondsInMinutes
 
     print("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
