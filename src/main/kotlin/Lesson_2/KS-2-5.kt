d0df021de5ea3c16afb50e104452e74031ab1115
NukeKB <alexey-seo@clientprosto.ru>
2025-12-04 10:44:22 +0300

    KS-2-5 исправил

--- src/main/kotlin/Lesson_2/KS-2-5.kt
+++ src/main/kotlin/Lesson_2/KS-2-5.kt
@@ -1,13 +1,12 @@
 package org.example.Lesson_2
 import kotlin.math.pow
 
-    const val DEPOSITE: Double = 70000.0
-
 fun main() {
+    var deposite: Double = 70000.0
     val yars: Double = 20.0
     val rateBank: Double = 16.7
     val bankRateFactor: Double = 1 + rateBank / 100
-    val result: Double = DEPOSITE * bankRateFactor.pow(yars)
+    val result: Double = deposite * bankRateFactor.pow(yars)
 
     println("%.3f".format(result))
 }
