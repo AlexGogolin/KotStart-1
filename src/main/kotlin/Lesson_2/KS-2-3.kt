7402128aef30daffb4e58e238187feb672fcb2fc
NukeKB <alexey-seo@clientprosto.ru>
2025-12-04 10:41:59 +0300

    KS-2-3 внес правки

--- src/main/kotlin/Lesson_2/KS-2-3.kt
+++ src/main/kotlin/Lesson_2/KS-2-3.kt
@@ -1,10 +1,15 @@
 package org.example.Lesson_2
 
+    const val HOURS_IN_DAY = 24
+    const val MINUTES_IN_HOUR = 60
+
 fun main() {
-    val startTrainInMinutes: Int = 9 * 60 + 39
+    val startHours = 9
+    val startMinutes = 39
+    val startTrainInMinutes: Int = startHours * MINUTES_IN_HOUR + startMinutes
     val timeOfWayInMinutes = 457
     val arrivalTotalMinutes = startTrainInMinutes + timeOfWayInMinutes
-    val remainingMinutes = arrivalTotalMinutes % (24 * 60)
+    val remainingMinutes = arrivalTotalMinutes % (HOURS_IN_DAY * MINUTES_IN_HOUR)
     val finishTimeInHour = remainingMinutes / 60
     val finishTimeInMinutes = remainingMinutes % 60
 
