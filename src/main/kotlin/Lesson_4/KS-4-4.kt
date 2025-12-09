5a2fde9bbebb6976d77705d0c6933f00c7971aa7
NukeKB <alexey-seo@clientprosto.ru>
2025-12-09 09:50:20 +0300

    KS-4-4

--- src/main/kotlin/Lesson_4/KS-4-4.kt
+++ src/main/kotlin/Lesson_4/KS-4-4.kt
@@ -3,7 +3,7 @@ package org.example.Lesson_4
 fun main() {
     val hands = 1
     val legs = 0
-    val day = 1
+    val day = 2
     val dayOfHands: Boolean = (day % 2) == hands
     val dayOfLegs: Boolean = (day % 2) == legs
 
