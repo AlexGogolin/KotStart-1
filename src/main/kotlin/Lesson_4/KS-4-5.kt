23ab2751811eddc14337f20572916129c9f3da98
NukeKB <alexey-seo@clientprosto.ru>
2026-01-19 10:17:11 +0300

    KS-5-2 Переписал некоторые условия на сколько понял

--- src/main/kotlin/Lesson_4/KS-4-5.kt
+++ src/main/kotlin/Lesson_4/KS-4-5.kt
@@ -3,14 +3,18 @@ package org.example.Lesson_4
 const val MIN_CREW = 55
 const val MAX_CREW = 70
 const val MIN_BOX_OF_FOOD = 50
+const val HAS_DAMAGE = true
+const val GOOD_WEATHER = true
 fun main() {
-    val noDamage = readln().toBoolean()
+    val damage = readln().toBoolean()
+    val isDamage = damage == HAS_DAMAGE
     val crew = readln().toInt()
     val boxOfFood = readln().toInt()
-    val goodWeather = readln().toBoolean()
+    val weather = readln().toBoolean()
+    val isGoodWeather = weather == GOOD_WEATHER
 
-    val sheepStartRecomendet = noDamage && (crew >= MIN_CREW && crew <= MAX_CREW) && (boxOfFood > MIN_BOX_OF_FOOD)
-    val sheepStartAlternative = (crew == MAX_CREW) && goodWeather && (boxOfFood >= MIN_BOX_OF_FOOD)
+    val sheepStartRecomendet = !isDamage && (crew >= MIN_CREW && crew <= MAX_CREW) && (boxOfFood > MIN_BOX_OF_FOOD)
+    val sheepStartAlternative = (crew == MAX_CREW) && isGoodWeather && (boxOfFood >= MIN_BOX_OF_FOOD)
     val sheepStart = sheepStartRecomendet || sheepStartAlternative
 
     println(sheepStart)
