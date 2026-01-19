f835972da955f0bb347d7ebcb1a7f097852695a2
NukeKB <alexey-seo@clientprosto.ru>
2026-01-19 12:51:55 +0300

    KS-5-4 Выполнено

---
+++ src/main/kotlin/Lesson_5/KS-5-4.kt
@@ -0,0 +1,21 @@
+package org.example.Lesson_5
+
+const val LOGIN: String = "Zaphod"
+const val PASSWORD: String = "anGalactic"
+fun main() {
+    println("Ваше имя: ")
+    val userName = readln()
+
+    when(userName) {
+        LOGIN -> {
+            println("Здравствуйте $userName , введите пароль:")
+            val userPassword = readln()
+            when(userPassword) {
+                PASSWORD -> println("Пользователь $userName, вам разрешено входить на борт корабля \"Heart of Gold\".")
+                else -> println("Пароль неверен, попробуйте еще раз")
+            }
+        }else -> println("Пользователь не найден")
+    }
+}
+
+
