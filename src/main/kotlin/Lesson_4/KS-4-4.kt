3e9c939921ce57d3709e83ee82726ea111608f55
NukeKB <alexey-seo@clientprosto.ru>
2026-01-19 09:39:29 +0300

    KS-5-2 надеюсь правильно понял, код сокращен, функции те же сохранены

--- src/main/kotlin/Lesson_4/KS-4-4.kt
+++ src/main/kotlin/Lesson_4/KS-4-4.kt
@@ -2,16 +2,14 @@ package org.example.Lesson_4
 
 fun main() {
     val hands = 1
-    val legs = 0
     val dayOfTraining = 5
     val dayOfHands: Boolean = (dayOfTraining % 2) == hands
-    val dayOfLegs: Boolean = (dayOfTraining % 2) == legs
 
     println(
         """
-            Упражнения для рук: $dayOfHands
-            Упражнения для ног:$dayOfLegs
-            Упражнения для спины: $dayOfLegs
+            Упражнения для рук: ${dayOfHands}
+            Упражнения для ног:${!dayOfHands}
+            Упражнения для спины: ${!dayOfHands}
             Упражнения для пресса: $dayOfHands
         """.trimIndent()
     )
