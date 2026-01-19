3e439b6b25d4d4aff59479e3c907720b04991607
NukeKB <alexey-seo@clientprosto.ru>
2026-01-19 09:55:48 +0300

    KS-5-2 Пока поменял название переменной, как поместить в константу то что заранее не известно не понимаю

--- src/main/kotlin/Lesson_4/KS-4-5.kt
+++ src/main/kotlin/Lesson_4/KS-4-5.kt
@@ -4,12 +4,12 @@ const val MIN_CREW = 55
 const val MAX_CREW = 70
 const val MIN_BOX_OF_FOOD = 50
 fun main() {
-    val nodamage = readln().toBoolean()
+    val noDamage = readln().toBoolean()
     val crew = readln().toInt()
     val boxOfFood = readln().toInt()
     val goodWeather = readln().toBoolean()
 
-    val sheepStartRecomendet = nodamage && (crew >= MIN_CREW && crew <= MAX_CREW) && (boxOfFood > MIN_BOX_OF_FOOD)
+    val sheepStartRecomendet = noDamage && (crew >= MIN_CREW && crew <= MAX_CREW) && (boxOfFood > MIN_BOX_OF_FOOD)
     val sheepStartAlternative = (crew == MAX_CREW) && goodWeather && (boxOfFood >= MIN_BOX_OF_FOOD)
     val sheepStart = sheepStartRecomendet || sheepStartAlternative
 
