6636010eee3bf19645f2efab2fdb612bc0ebfe64
NukeKB <alexey-seo@clientprosto.ru>
2026-01-19 16:59:38 +0300

    KS-5-6 выполнена, расчитывает все верно

---
+++ src/main/kotlin/Lesson_5/KS-5-6.kt
@@ -0,0 +1,22 @@
+package org.example.Lesson_5
+
+const val CENTIMETERS_PER_METER = 100
+fun main() {
+    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
+
+    println("Напишите ваш рост в сантиметрах:")
+    val userHeight = readln().toFloat()
+    println("Напишите ваш вес:")
+    val userWeight = readln().toFloat()
+
+    val userHeightInMeters = userHeight / CENTIMETERS_PER_METER
+    val imt = userWeight / (userHeightInMeters * userHeightInMeters)
+
+    val imtResult = when {
+        imt < 18.5 -> println("Недостаточная масса тела")
+        imt < 25 -> println("Нормальная масса тела")
+        imt < 30 -> println("Избыточная масса тела")
+        else -> println("Избыточная масса тела")
+    }
+    println("Ваш ИМТ равен: ${"%.2f".format(imt)}, у вас $imtResult")
+}
