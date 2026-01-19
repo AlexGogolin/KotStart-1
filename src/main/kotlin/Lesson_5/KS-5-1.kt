00bd9f4f7e0774f2539c53b213d9cff2756d101a
NukeKB <alexey-seo@clientprosto.ru>
2026-01-19 10:22:49 +0300

    KS-5-2 теперь все на переменных, не знаю надо ли их в константы выводить, пока оставил так

--- src/main/kotlin/Lesson_5/KS-5-1.kt
+++ src/main/kotlin/Lesson_5/KS-5-1.kt
@@ -1,8 +1,10 @@
 package org.example.Lesson_5
 
 fun main() {
-    val example = "Чему равно: 2 + 2 ?"
-    val correctAnswer = 4
+    val numberOne = 2
+    val numbertwo = 3
+    val example = "Чему равно: $numberOne + $numbertwo ?"
+    val correctAnswer = numberOne + numbertwo
 
     println("Докажите что вы не бот и решите пример. $example")
     val userAnswer = readln().toInt()
