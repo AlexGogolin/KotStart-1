a297e930dba00146c5975c22f7e1a8c9a1560e68
NukeKB <alexey-seo@clientprosto.ru>
2026-01-20 15:57:39 +0300

    KS-4-5 Убрал все дополнительные переменные, оставил только константы и сами условия отплатия

--- src/main/kotlin/Lesson_4/KS-4-5.kt
+++ src/main/kotlin/Lesson_4/KS-4-5.kt
@@ -3,18 +3,16 @@ package org.example.Lesson_4
 const val MIN_CREW = 55
 const val MAX_CREW = 70
 const val MIN_BOX_OF_FOOD = 50
-const val HAS_DAMAGE = true
+const val NO_DAMAGE = false
 const val GOOD_WEATHER = true
 fun main() {
-    val damage = readln().toBoolean()
-    val isDamage = damage == HAS_DAMAGE
+    val isdamage = readln().toBoolean()
     val crew = readln().toInt()
     val boxOfFood = readln().toInt()
     val weather = readln().toBoolean()
-    val isGoodWeather = weather == GOOD_WEATHER
 
-    val sheepStartRecomendet = !isDamage && (crew >= MIN_CREW && crew <= MAX_CREW) && (boxOfFood > MIN_BOX_OF_FOOD)
-    val sheepStartAlternative = (crew == MAX_CREW) && isGoodWeather && (boxOfFood >= MIN_BOX_OF_FOOD)
+    val sheepStartRecomendet = (isdamage == NO_DAMAGE) && (crew >= MIN_CREW && crew <= MAX_CREW) && (boxOfFood > MIN_BOX_OF_FOOD)
+    val sheepStartAlternative = (crew == MAX_CREW) && (weather == GOOD_WEATHER) && (boxOfFood >= MIN_BOX_OF_FOOD)
     val sheepStart = sheepStartRecomendet || sheepStartAlternative
 
     println(sheepStart)
