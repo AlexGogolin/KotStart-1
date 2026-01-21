24f8d4a6b3af8d63e871442649c5abbaf79a86eb
NukeKB <alexey-seo@clientprosto.ru>
2026-01-21 09:42:38 +0300

    KS-6-2 выполнено

---
+++ src/main/kotlin/Lesson_6/KS-6-2.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_6
+
+
+fun main(){
+    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
+
+    print("Введите количество секунд:")
+    var userSeconds = readln().toInt()
+    val seconds = userSeconds
+
+    while(userSeconds > 0){
+            println("Осталось секунд: ${userSeconds--}")
+            Thread.sleep(1000)
+    }
+    println("Прошло $seconds секунд")
+}
