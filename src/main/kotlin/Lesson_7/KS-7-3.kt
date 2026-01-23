cffaf3403b330df3cc1a5d56d46c4b3065d3f42f
NukeKB <alexey-seo@clientprosto.ru>
2026-01-23 12:33:48 +0300

    KS-7-3 По логике все работает как надо

---
+++ src/main/kotlin/Lesson_7/KS-7-3.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson_7
+
+fun main(){
+    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
+
+    println("Пожалуйста введите число:")
+    val userNumber = readln().toInt()
+
+    for(i in 0..userNumber step 2){
+        println(i)
+    }
+}
