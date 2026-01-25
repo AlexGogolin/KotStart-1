e92ed06bd31d8748a519b46bc766dadc3a342274
AlexGogolin <agogolin89@mail.ru>
2026-01-25 11:29:20 +0300

    KS-7-1 поправил

--- src/main/kotlin/Lesson_7/KS-7-1.kt
+++ src/main/kotlin/Lesson_7/KS-7-1.kt
@@ -1,17 +1,16 @@
 package org.example.Lesson_7
 
-fun main(){
+fun main() {
     System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
 
     var password: String = ""
-
-    for(i in 1..6 ){
-        if(i % 2 == 0){
-            val intRange = (1..9).random()
-            password += intRange
-        }else{
-            val charRange = ('a'..'z').random()
-            password += charRange
+    val intRange = 1..10
+    val charRange = 'a'..'z'
+    for (i in 1..6) {
+        if (i % 2 == 0) {
+            password += intRange.random().toString()
+        } else {
+            password += charRange.random()
         }
     }
     println(password)
