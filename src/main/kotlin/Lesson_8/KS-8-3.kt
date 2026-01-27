8dd3ffd54074c01015bc7aa8577d694bc80bfa1f
NukeKB <alexey-seo@clientprosto.ru>
2026-01-27 14:33:43 +0300

    KS-8-3 Поправил недочеты, по поводу in в конструкте if буду знать, использовал тот метод который знал и он работал)

--- src/main/kotlin/Lesson_8/KS-8-3.kt
+++ src/main/kotlin/Lesson_8/KS-8-3.kt
@@ -3,12 +3,12 @@ package org.example.Lesson_8
 fun main() {
     System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
 
-    val arrayOfIngridients = arrayOf("мясо", "свекла", "томат", "соль", "перец", "капуста", "лук", "морковь").toList()
+    val arrayOfIngredients = arrayOf("мясо", "свекла", "томат", "соль", "перец", "капуста", "лук", "морковь")
 
     println("Напишите интересующий вас ингредиент: ")
     val searchIngridient = readln().lowercase()
 
-    if (arrayOfIngridients.contains(searchIngridient)) {
+    if (searchIngridient in arrayOfIngredients) {
         println("Ингредиент $searchIngridient есть в составе этого блюда")
         return
     }
