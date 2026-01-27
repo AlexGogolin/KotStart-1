469fb8619fcc677151b2075ac409638483d8dd6c
NukeKB <alexey-seo@clientprosto.ru>
2026-01-27 16:03:40 +0300

    KS-9-3 Готово, сначала полез в списки типа map, ничего не получалось, потом перечитал задачу и сделал так)

---
+++ src/main/kotlin/Lesson_9/KS-9-3.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_9
+
+fun main() {
+    System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
+
+    val listOfIngredients = listOf(2, 50, 15)
+    println("Сколько порций вы хотели бы приготовить?")
+    val userAnswer = readln().toInt()
+
+    val ingerientsForPersons = listOfIngredients.map {
+        it * userAnswer
+    }
+
+    println("Для приготовления $userAnswer порций, вам понадобится: Яйца - ${ingerientsForPersons[0]} шт., Молоко - ${ingerientsForPersons[1]} мл., Сливочное масло - ${ingerientsForPersons[2]} гр.")
+}
