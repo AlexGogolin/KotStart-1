23a5ce27da4fcacd420493ddefc22bb67f44eb37
NukeKB <alexey-seo@clientprosto.ru>
2026-02-02 16:51:56 +0300

    KS-11-1 Надеюсь правильно понял задачу

---
+++ src/main/kotlin/Lesson_11/KS-11-1.kt
@@ -0,0 +1,28 @@
+package org.example.Lesson_11
+
+fun main() {
+
+    val user1 = User(
+        id = 1,
+        login = "Alberto",
+        password = "123456",
+        email = "alberto@mail.com",
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "Pepe",
+        password = "qwerty",
+        email = "lama@mail.com",
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.email)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.email)
+}
