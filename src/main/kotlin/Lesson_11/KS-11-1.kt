2ce69151ec107fb349db38eeccac261123b0207a
NukeKB <alexey-seo@clientprosto.ru>
2026-02-03 17:28:21 +0300

    KS-11-1 перенес

--- src/main/kotlin/Lesson_11/KS-11-1.kt
+++ src/main/kotlin/Lesson_11/KS-11-1.kt
@@ -26,3 +26,12 @@ fun main() {
     println(user2.password)
     println(user2.email)
 }
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {
+
+}
