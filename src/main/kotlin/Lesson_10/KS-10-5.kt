5cd097b7202ec6e59127d3ecc49d359b578f1566
NukeKB <alexey-seo@clientprosto.ru>
2026-02-04 15:44:56 +0300

    KS-10-5 поправил

--- src/main/kotlin/Lesson_10/KS-10-5.kt
+++ src/main/kotlin/Lesson_10/KS-10-5.kt
@@ -4,8 +4,9 @@ const val LOGIN = "Owner"
 const val PASSWORD = "123456"
 fun main() {
     System.setOut(java.io.PrintStream(System.`out`, true, "UTF-8"))
+
     println("Пожалуйста авторизуйтесь, введите ваш логин и пароль:")
-    val myToken = isAuthorized(readln(), readln())
+    val myToken = authorizeUser(readln(), readln())
     val cart = getCart(myToken)
     if (cart != null) {
         println("Ваш список товаров в корзине: $cart")
@@ -15,19 +16,17 @@ fun main() {
 
 }
 
-fun tokenGenerator(): String {
-    val token = mutableListOf<Char>()
+fun generateToken(): String = buildString {
     val allChars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-    while (token.size < 32) {
-        token += allChars.random()
+    repeat(32) {
+        append(allChars.random())
     }
-    return token.joinToString("")
 }
 
-fun isAuthorized(userlogin: String, userpassword: String): String? {
+fun authorizeUser(userlogin: String, userpassword: String): String? {
     return if (userlogin == LOGIN && userpassword == PASSWORD) {
-        tokenGenerator()
-    }else null
+        generateToken()
+    } else null
 }
 
 fun getCart(token: String?): String? {
