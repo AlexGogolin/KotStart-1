b3492796591f46f731a98bac1da547965f3f36b6
AlexGogolin <agogolin89@mail.ru>
2026-02-14 11:51:48 +0300

    KS-13-4 переделал и кое-какие вещи прикрутил так сказать от себя, знаю что может по задаче не верно будет, но начал прикайфовывать от копания в коде и захотелось что-то докрутить)

--- src/main/kotlin/Lesson_13/KS-13-4.kt
+++ src/main/kotlin/Lesson_13/KS-13-4.kt
@@ -6,32 +6,37 @@ fun main() {
     System.setOut(PrintStream(System.`out`, true, "UTF-8"))
 
     val phoneBook = mutableListOf<AnotherUserStorage3>()
-    println("Внесите данные!")
-    println("Ваше имя:")
-    val userName = readln()
-    println("Ваш номер телефона:")
-    val userPhone = readln().toULongOrNull()
-    println("Место работы:")
-    val userCompany: String? = readln().ifBlank { null }
-
-    if (userPhone != null) {
-        val newUser = AnotherUserStorage3(userName, userPhone, userCompany)
-        phoneBook.add(newUser)
-        newUser.userInfo()
-    } else {
-        println("Введите номер телефона")
+    while (true) {
+        println("Ваше имя:")
+        val userName = readln()
+        if (userName.isBlank()) {
+            break
+        }
+        println("Ваш номер телефона:")
+        val userPhone = readln().toLongOrNull()
+        if (userPhone != null) {
+            println("Место работы:")
+            val userCompany: String? = readln().ifBlank { null }
+            val newUser = AnotherUserStorage3(userName, userPhone, userCompany)
+            phoneBook.add(newUser)
+            newUser.userInfo()
+        } else {
+            println("Вы не ввели номер телефона, учетная запись не будет добавлена, попробуйте еще раз!")
+        }
     }
 
+    println("-- Итоговый список контактов --")
+    phoneBook.forEach { it.userInfo() }
 
 }
 
 class AnotherUserStorage3(
     val name: String,
-    val phoneNumber: ULong? = null,
+    val phoneNumber: Long,
     val company: String? = null
 ) {
     fun userInfo() {
-        println("Имя пользователя:$name\nНомер телефона:$phoneNumber\nМесто работы:$company")
+        println("Контакт: \nИмя пользователя:$name\nНомер телефона:$phoneNumber\nМесто работы:$company")
     }
 }
 
