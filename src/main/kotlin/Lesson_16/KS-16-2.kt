d042cb98b8128f958203165bef7f5784cc494e33
NukeKB <alexey-seo@clientprosto.ru>
2026-03-02 08:40:09 +0300

    KS-16-2 поправил

--- src/main/kotlin/Lesson_16/KS-16-2.kt
+++ src/main/kotlin/Lesson_16/KS-16-2.kt
@@ -1,6 +1,7 @@
 package org.example.Lesson_16
+import kotlin.math.pow
 
-
+const val PI: Double = 3.14
 fun main() {
 
     val figure = Circle(312)
@@ -13,13 +14,12 @@ fun main() {
 class Circle(
     private val radius: Int,
 ) {
-    private val PI: Double = 3.14
 
     fun calculatePerimeter(): Double {
         return 2 * PI * radius
     }
 
     fun calculateArea(): Double {
-        return PI * radius * radius
+        return PI * radius.toDouble().pow(2)
     }
 }
