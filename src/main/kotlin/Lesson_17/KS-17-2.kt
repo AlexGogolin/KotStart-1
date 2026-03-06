435d48dde3d2e873827186d24d8df387b829ce92
NukeKB <alexey-seo@clientprosto.ru>
2026-03-06 14:26:04 +0300

    KS-17-2 т.е. что-то такое? Мы просто в сет вне зависимости от принятого value отдаем всегда предупреждение?

--- src/main/kotlin/Lesson_17/KS-17-2.kt
+++ src/main/kotlin/Lesson_17/KS-17-2.kt
@@ -1,10 +1,7 @@
 package org.example.Lesson_17
 
-import java.io.PrintStream
-
 
 fun main() {
-    System.setOut(PrintStream(System.`out`, true, "UTF-8"))
     val ship = Ship("Сокол тысячилетия", 500, "Альдебаран")
     println(ship.name)
     ship.name = "Ястреб тысячелетия"
@@ -20,6 +17,6 @@ class Ship(
     var name: String = shipName
         get() = field
         set(value) {
-                println("$name - имя корабля данное ему при постройке и менять его нельзя!")
+            println("$name - имя корабля данное ему при постройке и менять его нельзя!")
         }
 }
