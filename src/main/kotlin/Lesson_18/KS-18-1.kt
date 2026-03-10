8e6b1806db5db286157836652ef815391f1b632d
NukeKB <alexey-seo@clientprosto.ru>
2026-03-10 08:48:33 +0300

    KS-18-1 правки

--- src/main/kotlin/Lesson_18/KS-18-1.kt
+++ src/main/kotlin/Lesson_18/KS-18-1.kt
@@ -1,9 +1,9 @@
 package org.example.Lesson_18
 
 fun main() {
-    val order1 = Order(1)
-    order1.productOrdered("Молоко")
-    order1.productOrdered(listOf("Кофе", "Сахар", "Сливки"))
+    val order = Order(1)
+    order.productOrdered("Молоко")
+    order.productOrdered(listOf("Кофе", "Сахар", "Сливки"))
 }
 
 class Order(
