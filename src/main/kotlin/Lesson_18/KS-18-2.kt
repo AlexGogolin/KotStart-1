204e3f8515e0acdc3ba8eff26db4cad82e2530d5
NukeKB <alexey-seo@clientprosto.ru>
2026-03-11 16:30:06 +0300

    KS-18-2 исправил

--- src/main/kotlin/Lesson_18/KS-18-2.kt
+++ src/main/kotlin/Lesson_18/KS-18-2.kt
@@ -2,9 +2,9 @@ package org.example.Lesson_18
 
 fun main() {
 
-    val dice1: Dice = Tetrahedron("Четырехгранник")
-    val dice2: Dice = Hexagon("Шестигранник")
-    val dice3: Dice = Octahedron("Восьмигранник")
+    val dice1: Dice = Tetrahedron()
+    val dice2: Dice = Hexagon()
+    val dice3: Dice = Octahedron()
 
     val listOfDices: List<Dice> = listOf(dice1, dice2, dice3)
     listOfDices.forEach {
@@ -12,32 +12,21 @@ fun main() {
     }
 }
 
-abstract class Dice(
-    val type: String,
-) {
-    abstract fun rollOfDice()
+abstract class Dice {
+    abstract val facesCount: Int
+    fun rollOfDice() {
+        println("Кубик брошен, выпало ${(1..facesCount).random()}")
+    }
 }
 
 class Tetrahedron(
-    type: String
-) : Dice(type) {
-    override fun rollOfDice() {
-        println("Кубик $type брошен, выпало ${(1..4).random()}")
-    }
-}
+    override val facesCount: Int = 4,
+) : Dice()
 
 class Hexagon(
-    type: String
-) : Dice(type) {
-    override fun rollOfDice() {
-        println("Кубик $type брошен, выпало ${(1..6).random()}")
-    }
-}
+    override val facesCount: Int = 6,
+) : Dice()
 
 class Octahedron(
-    type: String
-) : Dice(type) {
-    override fun rollOfDice() {
-        println("Кубик $type брошен, выпало ${(1..8).random()}")
-    }
-}
+    override val facesCount: Int = 8,
+) : Dice()
