d4410a7db8e8095cabd7e918bb23a54a5381ccad
AlexGogolin <agogolin89@mail.ru>
2026-03-18 10:33:28 +0300

    KS-20-5 правки

--- src/main/kotlin/Lesson_20/KS-20-5.kt
+++ src/main/kotlin/Lesson_20/KS-20-5.kt
@@ -1,9 +1,6 @@
 package org.example.Lesson_20
 
-import java.io.PrintStream
-
 fun main() {
-    System.setOut(PrintStream(System.`out`, true, "UTF-8"))
     val bender = Robot()
 
     bender.say()
@@ -12,14 +9,21 @@ fun main() {
 
 }
 
-class Robot{
-    val phrases: List<String> = listOf("Здарова кожанный", "Сравните свою жизнь с моей, а потом убейте себя!", "Позеры! Я ненавидел Зойдберга еще до того, как это стало модным.", "Куда подевался Фрай?", "Я люблю вас, мешки с костями")
+class Robot {
+    val phrases: List<String> = listOf(
+        "Здарова кожанный",
+        "Сравните свою жизнь с моей, а потом убейте себя!",
+        "Позеры! Я ненавидел Зойдберга еще до того, как это стало модным.",
+        "Куда подевался Фрай?",
+        "Я люблю вас, мешки с костями"
+    )
     var speechModifier: (String) -> String = { it }
     val randomPhrase = phrases.random()
-    fun say(){
+    fun say() {
         println(speechModifier(randomPhrase))
     }
-    fun setModifier (modifer: (String) -> String){
+
+    fun setModifier(modifer: (String) -> String) {
         speechModifier = modifer
     }
 }
