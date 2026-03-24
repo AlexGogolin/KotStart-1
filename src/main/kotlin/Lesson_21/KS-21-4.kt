12bcd4f5359af7247263a07861a7cd3964482cf6
NukeKB <alexey-seo@clientprosto.ru>
2026-03-24 17:09:30 +0300

    KS-21-4 правки

--- src/main/kotlin/Lesson_21/KS-21-4.kt
+++ src/main/kotlin/Lesson_21/KS-21-4.kt
@@ -2,16 +2,8 @@ package org.example.Lesson_21
 
 import java.io.File
 
-fun main() {
-
-}
-
 fun File.addInfoToFile(text: String) {
-    if (this.exists()) {
-        val infoInFile = this.readText()
-        val addetToFile = text.lowercase() + "\n$infoInFile"
-        this.writeText(addetToFile)
-    } else {
-        this.writeText(text.lowercase())
-    }
+    val infoInFile = this.readText()
+    val addedToFile = text.lowercase() + "\n$infoInFile"
+    this.writeText(addedToFile)
 }
